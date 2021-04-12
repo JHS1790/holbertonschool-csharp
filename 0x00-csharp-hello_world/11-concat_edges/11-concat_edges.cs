f609253ce2a5f6ba01efe6a4390d50bb8e5cc55d
JHS1790 <1790@holbertonschool.com>
2021-04-12 13:22:21 -0500

    go at 11

---
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿  using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Remove(0, 49).Remove(15) + str.Remove(0, 79).Remove(12) + str.Remove(0, 118).Remove(4) + str.Remove(2);
+		Console.WriteLine(str);
+        }
+}
