286f93b6a7bde7117ea62a9825313b2277ce51d4
JHS1790 <1790@holbertonschool.com>
2021-04-13 11:17:00 -0500

    go on 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = i + 1; j <= 9; j++)
+                    Console.Write("{0}{1}, ", i, j);
+            Console.WriteLine("89");
+        }
+    }
+}
