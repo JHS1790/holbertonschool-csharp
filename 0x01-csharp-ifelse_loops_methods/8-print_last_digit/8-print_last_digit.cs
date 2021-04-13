8290bcca508297583268c307d971630d214e95e1
JHS1790 <1790@holbertonschool.com>
2021-04-13 11:29:14 -0500

    go on 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last_digit = number % 10;
+        if (last_digit < 0)
+            last_digit = last_digit * -1;
+        Console.Write(last_digit);
+        return (last_digit);
+    }
+}
