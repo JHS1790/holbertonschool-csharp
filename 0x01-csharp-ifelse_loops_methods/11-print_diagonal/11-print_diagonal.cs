ce90d05981cb101b79e39a5f10272c54eea37604
JHS1790 <1790@holbertonschool.com>
2021-04-13 11:44:39 -0500

    Possible go on 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < length; i++)
+            for (int j = 0; j < i; j++)
+                Console.Write(" ");
+            Console.WriteLine("\\");
+    }
+}
