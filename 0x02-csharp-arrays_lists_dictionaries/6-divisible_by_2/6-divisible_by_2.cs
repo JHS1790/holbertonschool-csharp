516a1b1815e092251acca8ae16d1fb3625f47bdf
JHS1790 <1790@holbertonschool.com>
2021-04-14 12:14:16 -0500

    go on 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> div2List = new List<bool>();
+        foreach (int number in myList)
+        {
+            if (number % 2 == 0)
+                div2List.Add(true);
+            else
+                div2List.Add(false);
+        }
+        return div2List;
+    }
+}
