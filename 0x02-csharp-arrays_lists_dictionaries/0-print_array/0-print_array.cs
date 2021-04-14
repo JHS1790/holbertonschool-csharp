08e9d751ccaf3bc500f8b1ed0e115fd157d7a2e3
JHS1790 <1790@holbertonschool.com>
2021-04-14 11:16:24 -0500

    go on 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            Console.Write(i);
+            if (i != size - 1)
+                Console.Write(" ");
+            else
+                Console.Write("\n");
+        }
+
+        if (size == 0)
+            Console.Write("\n");
+
+        return array;
+    }
+}
