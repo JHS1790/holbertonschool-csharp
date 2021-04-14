7d57296c70e8776dbdec70b5e0019ee86e172e03
JHS1790 <1790@holbertonschool.com>
2021-04-14 14:37:03 -0500

    go on 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    array[i, j] = 0;
+            array[2, 2] = 1;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write("{0}{1}", array[i, j], j == array.GetLength(1) - 1 ? "":" ");
+                Console.Write("\n");
+            }
+        }
+    }
+}
