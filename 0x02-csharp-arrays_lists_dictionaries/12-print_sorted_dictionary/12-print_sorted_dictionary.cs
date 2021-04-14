61878a4b68009de7d883b29cf1f751dda4edc0fc
JHS1790 <1790@holbertonschool.com>
2021-04-14 13:49:53 -0500

    go on 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> Keys = new List<string>();
+        List<object> Values = new List<object>();
+
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            Keys.Add(pair.Key);
+            Values.Add(pair.Value);
+        }
+
+        var newdict = new Dictionary<string, object>();
+        for (int i = 0; i < Keys.Count; i++) 
+            newdict.Add(Keys[i], Values[i]);
+        Keys.Sort();
+        for (int i = 0; i < Keys.Count; i++)
+        {
+            Console.WriteLine($"{Keys[i]}: {newdict[Keys[i]]}");
+        }
+    }
+}
