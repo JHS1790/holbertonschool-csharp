9c0ba55a6f21371fb4f99c069c1c1b9dc7e26398
JHS1790 <1790@holbertonschool.com>
2021-04-14 14:13:54 -0500

    go on 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return "None";
+        string best_score_key = "Adam";
+        int best_score_value = 0;
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            if (best_score_value < pair.Value)
+            {
+                best_score_key = pair.Key;
+                best_score_value = pair.Value;
+            }
+        }
+        return best_score_key;
+    }
+}
