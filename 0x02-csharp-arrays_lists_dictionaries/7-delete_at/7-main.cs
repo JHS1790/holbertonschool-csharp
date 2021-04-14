3a974ce00f01f5faa12b1c288dc0f2e4b1db9b62
JHS1790 <1790@holbertonschool.com>
2021-04-14 15:09:39 -0500

    took out -1

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-main.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-main.cs
@@ -12,18 +12,10 @@ class Program
 
         Console.WriteLine("----------");
 
-        List.DeleteAt(myList, 10);
+        List.DeleteAt(myList, 5);
 
         foreach (int i in myList)
             Console.WriteLine(i);
 
-        Console.WriteLine("----------");
-
-        List.DeleteAt(myList, 2);
-        List.DeleteAt(myList, -1);
-
-        Console.WriteLine("----------");
-
-        List.DeleteAt(null, 2);
     }
 }
