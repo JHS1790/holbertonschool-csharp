16cbcd6934bc9aaa339436de5154392258b439ed
JHS1790 <1790@holbertonschool.com>
2021-04-15 12:36:24 -0500

    go on 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        SortedSet<int> commonSet = new SortedSet<int>();
+
+        for (int i = 0; i < list1.Count; i++)
+            for (int j = 0; j < list2.Count; j++)
+                if (list1[i] == list2[j])
+                {
+                    commonSet.Add(list1[i]);
+                    break;
+                }
+        List<int> commonList = new List<int>(commonSet);
+        return commonList;
+    }
+}
