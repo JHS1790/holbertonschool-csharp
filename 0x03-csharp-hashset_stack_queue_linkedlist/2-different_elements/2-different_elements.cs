f26720115eff492cca7325eb88fff424e225a06e
JHS1790 <1790@holbertonschool.com>
2021-04-15 12:50:48 -0500

    go on 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        SortedSet<int> uncommonSet = new SortedSet<int>();
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            bool is_common = false;
+            for (int j = 0; j < list2.Count; j++)
+            {
+                if (list1[i] == list2[j])
+                {
+                    is_common = true;
+                    break;
+                }
+            }
+            if (is_common == false)
+                uncommonSet.Add(list1[i]);
+        }
+        for (int i = 0; i < list2.Count; i++)
+        {
+            bool is_common = false;
+            for (int j = 0; j < list1.Count; j++)
+            {
+                if (list2[i] == list1[j])
+                {
+                    is_common = true;
+                    break;
+                }
+            }
+            if (is_common == false)
+                uncommonSet.Add(list2[i]);
+        }
+        List<int> uncommonList = new List<int>(uncommonSet);
+        return uncommonList;
+
+    }
+}
