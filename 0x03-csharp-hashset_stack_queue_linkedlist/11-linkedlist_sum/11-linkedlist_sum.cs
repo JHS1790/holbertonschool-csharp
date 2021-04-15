b197c4990595936b38da2929c48b6738afead1ac
JHS1790 <1790@holbertonschool.com>
2021-04-15 14:32:24 -0500

    go on 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int listSum = 0;
+        LinkedListNode<int> navigator = myLList.First;
+        while (navigator != null)
+        {
+            listSum += navigator.Value;
+            navigator = navigator.Next;
+        }
+        return listSum;
+    }
+}
