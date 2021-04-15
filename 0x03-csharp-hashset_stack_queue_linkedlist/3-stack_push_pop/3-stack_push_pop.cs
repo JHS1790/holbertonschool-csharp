3ed8d309d1680841942a696b8c9210affeff9c57
JHS1790 <1790@holbertonschool.com>
2021-04-15 13:11:28 -0500

    go on 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        try
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        catch
+        {
+            Console.WriteLine($"Stack is empty");
+        }
+        Console.WriteLine($"Stack contains {search}: {aStack.Contains(search)}");
+        if (aStack.Contains(search))
+        {
+            bool is_search = false;
+            while (is_search == false)
+            {
+                if (aStack.Peek() == search)
+                    is_search = true;
+                aStack.Pop();
+            }
+        }
+        if (newItem != null)
+            aStack.Push(newItem);
+        return aStack;
+    }
+}
