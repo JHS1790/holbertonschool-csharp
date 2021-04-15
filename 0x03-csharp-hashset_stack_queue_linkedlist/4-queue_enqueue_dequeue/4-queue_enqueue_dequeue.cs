77f9b8b805535b5cd176d72ab81337e179e9d568
JHS1790 <1790@holbertonschool.com>
2021-04-15 13:50:17 -0500

    go on 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        try
+        {
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        }
+        catch
+        {
+            Console.WriteLine($"Queue is empty");
+        }
+        if (newItem != null)
+            aQueue.Enqueue(newItem);
+        Console.WriteLine($"Queue contains {search}: {aQueue.Contains(search)}");
+        if (aQueue.Contains(search))
+        {
+            bool is_search = false;
+            while (is_search == false)
+            {
+                if (aQueue.Peek() == search)
+                    is_search = true;
+                aQueue.Dequeue();
+            }
+        }
+        return aQueue;
+    }
+}
