289732966575ef10f2c5f44c55e2b00742c82fab
JHS1790 <1790@holbertonschool.com>
2021-04-15 15:10:29 -0500

    go on 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> navigator = myLList.First;
+        while (navigator != null)
+        {
+            if (navigator.Next != null)
+            {
+                if ((n >= navigator.Value) && (n < navigator.Next.Value))
+                {
+                    myLList.AddBefore(navigator, n);
+                    return navigator.Previous;
+                }
+            }
+            navigator = navigator.Next;
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
+    }
+}
