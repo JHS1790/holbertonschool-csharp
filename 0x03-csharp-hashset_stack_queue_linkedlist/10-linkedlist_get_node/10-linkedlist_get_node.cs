10e06f217df0c4c2994b8929bc015b93503b676c
JHS1790 <1790@holbertonschool.com>
2021-04-15 14:29:40 -0500

    go on 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int navIndex = 0;
+        LinkedListNode<int> navigator = myLList.First;
+        while (navigator != null)
+        {
+            if (navIndex == n)
+                return navigator.Value;
+            navIndex++;
+            navigator = navigator.Next;
+        }
+        return 0;
+    }
+}
