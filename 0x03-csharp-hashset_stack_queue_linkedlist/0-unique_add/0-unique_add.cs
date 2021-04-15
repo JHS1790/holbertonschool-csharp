b1e2cf23ec5e538e527a39d47979636dfa41f5c4
JHS1790 <1790@holbertonschool.com>
2021-04-15 11:10:39 -0500

    first go on 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqList = new HashSet<int>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            uniqList.Add(myList[i]);
+        }
+        return uniqList.Sum();
+    }
+}
