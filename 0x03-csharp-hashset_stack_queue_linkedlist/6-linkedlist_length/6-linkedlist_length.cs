462550c4f1a27c8f232ba880404231695c2f6014
JHS1790 <1790@holbertonschool.com>
2021-04-15 14:11:03 -0500

    go on 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        LinkedListNode<int> navigator = myLList.First;
+        while (navigator != null)
+        {
+            count++;
+            navigator = navigator.Next;
+        }
+        return count;
+    }
+}
