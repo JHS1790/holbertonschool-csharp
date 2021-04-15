86473cf0623b605c3cb2078c799f969a42b2bcaf
JHS1790 <1790@holbertonschool.com>
2021-04-15 14:20:07 -0500

    go on 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int navIndex = 0;
+        LinkedListNode<int> navigator = myLList.First;
+        while (navigator != null)
+        {
+            if (navigator.Value == value)
+                return navIndex;
+            navIndex++;
+            navigator = navigator.Next;
+        }
+        return -1;
+    }
+}
