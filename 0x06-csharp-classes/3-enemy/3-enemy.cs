19cebbe0b23378dda8492fa63d0b0facea599023
JHS1790 <1790@holbertonschool.com>
2021-04-27 10:24:30 -0500

    go on 3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            this.health = value;
+        }
+    }
+}
