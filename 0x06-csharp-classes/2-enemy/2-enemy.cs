c64860b4951f675fe332f6d3d4a89c08a5a38816
JHS1790 <1790@holbertonschool.com>
2021-04-27 13:46:58 -0500

    switched constructors

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -7,15 +7,15 @@ namespace Enemies
     {
         public int health;
 
-        public Zombie()
-        {
-            this.health = 0;
-        }
         public Zombie(int value)
         {
             if (value < 0)
                 throw new ArgumentException("Health must be greater than or equal to 0");
             this.health = value;
+        }
+        public Zombie()
+        {
+            this.health = 0;
         }
     }
 }
