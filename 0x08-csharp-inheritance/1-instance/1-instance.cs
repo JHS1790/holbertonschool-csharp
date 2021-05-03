57cf50e92e7ec59f8c4438864a025c59e2163bcf
JHS1790 <1790@holbertonschool.com>
2021-05-03 11:32:15 -0500

    go on 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+            return true;
+        else
+            return false;
+    }
+}
