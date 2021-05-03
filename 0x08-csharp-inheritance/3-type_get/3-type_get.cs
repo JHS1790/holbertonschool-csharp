e926527806e72be8295220c0ff4db5899ad294c6
JHS1790 <1790@holbertonschool.com>
2021-05-03 12:19:25 -0500

    go on 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Globalization;
+using System.Text;
+
+class Obj
+{
+    ///<summary>a method that prints the names of the available properties and methods of an object</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+        IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+        Console.WriteLine($"{t.Name} Properties:");
+        foreach (PropertyInfo p in pList)
+        {
+            if (p.Name.StartsWith("System."))
+                continue;
+            Console.WriteLine($"{p.Name}");
+        }
+        Console.WriteLine($"{t.Name} Methods:");
+        foreach (MethodInfo m in mList)
+        {
+            if (m.Name.StartsWith("System."))
+                continue;
+            Console.WriteLine($"{m.Name}");
+        }
+    }
+}
