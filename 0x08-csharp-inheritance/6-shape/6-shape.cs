f44cfdaf4b41104159126a2389c28c39ebd5d556
JHS1790 <1790@holbertonschool.com>
2021-05-03 13:52:54 -0500

    go on 6

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Shape
+{
+    ///<summary>Throws an NotImplementedException with the message Area() is not implemented</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    ///wide boi
+    private int width;
+    ///tall boi
+    private int height;
+
+    ///<summary>how wide is da boi</summary>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+    
+    ///<summary>how tall is da boi</summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+}
