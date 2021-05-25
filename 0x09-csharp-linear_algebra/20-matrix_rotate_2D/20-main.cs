dea5c43da405ccf3a3f79bc8c918bf835dc538ce
JHS1790 <1790@holbertonschool.com>
2021-05-25 11:59:35 -0500

    a

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] matrix = { { 1, 2 }, { 3, 4 } };
+        double theta = -1.57;
+        double[,] result;
+
+        result = MatrixMath.Rotate2D(matrix, theta);
+
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j]);
+                if (j != result.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,54 @@
+using System;
+
+class MatrixMath
+{
+	public static double[,] Rotate2D(double[,] matrix, double angle)
+	{
+		if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+			return new double[,] { { -1 } };
+
+		double[,] rotation_matrix = new double[,] { { Math.Cos(angle), -(Math.Sin(angle)) }, { Math.Sin(angle), Math.Cos(angle) } };
+
+		double[,] answer = new double[2, 2];
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				answer[i, j] = Math.Round(DotProduct(RowBuilder(rotation_matrix, i), ColumnBuilder(matrix, j)), 2);
+
+		return answer;
+	}
+
+	public static double[] RowBuilder(double[,] matrix, int rowNumber)
+	{
+		double[] row = new double[matrix.GetLength(1)];
+		
+		for (int i = 0; i < matrix.GetLength(1); i++)
+			row[i] = matrix[rowNumber, i];
+
+		return row;
+	}
+	
+	public static double[] ColumnBuilder(double[,] matrix, int columnNumber)
+	{
+		double[] column = new double[matrix.GetLength(0)];
+		
+		for (int i = 0; i < matrix.GetLength(0); i++)
+			column[i] = matrix[i, columnNumber];
+
+		return column;
+	}
+
+	public static double DotProduct(double[] vector1, double[] vector2)
+	{
+		double[] interim_answer = new double[vector1.Length];
+		double answer = 0;
+
+		for (int i = 0; i < vector1.Length; i++)
+			interim_answer[i] = vector1[i] * vector2[i];
+
+		foreach (double part in interim_answer)
+			answer += part;
+
+		return answer;
+	}
+}
