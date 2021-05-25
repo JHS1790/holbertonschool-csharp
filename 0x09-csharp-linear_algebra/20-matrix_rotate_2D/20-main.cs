8b5bc0e04fddd15fa43da99497e4ef1d47728333
JHS1790 <1790@holbertonschool.com>
2021-05-25 14:43:29 -0500

    b

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-main.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-main.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 
-class Program
+static void Main(string[] args)
 {
-    static void Main(string[] args)
-    {
-        double[,] matrix = { { 1, 2 }, { 3, 4 } };
-        double[,] result;
-        
-        result = MatrixMath.Rotate2D(matrix, 2);
+    double[,] matrix = { { 1, 2 }, { 3, 4 } };
+    double[,] result;
+    
+    result = MatrixMath.Rotate2D(matrix, 2);
 
-        for (int i = 0; i < result.GetLength(0); i++)
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
         {
-            for (int j = 0; j < result.GetLength(1); j++)
-            {
-                Console.Write(result[i, j]);
-                if (j != result.GetLength(1) - 1)
-                    Console.Write(", ");
-            }
-            Console.WriteLine();
+            Console.Write(result[i, j]);
+            if (j != result.GetLength(1) - 1)
+                Console.Write(", ");
         }
+        Console.WriteLine();
     }
 }
