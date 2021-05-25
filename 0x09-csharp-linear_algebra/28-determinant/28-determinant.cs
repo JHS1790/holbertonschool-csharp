9f3cedcdaad2852f0eb9ccbdba639f0b77d0570d
JHS1790 <1790@holbertonschool.com>
2021-05-25 15:21:31 -0500

    d

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -4,9 +4,9 @@ class MatrixMath
 {
 	public static double Determinant(double[,] matrix)
 	{
-		if (matrix.Length == 4)
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
 			return Math.Round(Determinant2d(matrix), 2);
-		else if (matrix.Length == 9)
+		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
 			return Math.Round(Determinant3d(matrix), 2);
 		else
 			return -1;
@@ -22,6 +22,56 @@ class MatrixMath
 		double[,] a = new double[,] { { matrix[1,1] , matrix[1,2] } , { matrix[2,1] , matrix[2,2] } };
 		double[,] b = new double[,] { { matrix[1,0] , matrix[1,2] } , { matrix[2,0] , matrix[2,2] } };
 		double[,] c = new double[,] { { matrix[1,0] , matrix[1,1] } , { matrix[2,0] , matrix[2,1] } };
-		return ( ( matrix[0,0] * Determinant2d(a) ) - ( matrix[1,0] * Determinant2d(b) ) - ( matrix[2,0] * Determinant2d(c) ) );
+		/*
+		Console.WriteLine("Matrix:");
+		for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j]);
+                if (j != matrix.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    	Console.WriteLine("----------");
+		Console.WriteLine("a subatrix:");
+		for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                Console.Write(a[i, j]);
+                if (j != a.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    	Console.WriteLine("----------");
+		Console.WriteLine("b subatrix:");
+		for (int i = 0; i < b.GetLength(0); i++)
+        {
+            for (int j = 0; j < b.GetLength(1); j++)
+            {
+                Console.Write(b[i, j]);
+                if (j != b.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    	Console.WriteLine("----------");
+		Console.WriteLine("c subatrix:");
+		for (int i = 0; i < c.GetLength(0); i++)
+        {
+            for (int j = 0; j < c.GetLength(1); j++)
+            {
+                Console.Write(c[i, j]);
+                if (j != c.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    	Console.WriteLine("----------");
+		*/
+		return ( ( matrix[0,0] * Determinant2d(a) ) - ( matrix[0,1] * Determinant2d(b) ) - ( matrix[0,2] * Determinant2d(c) ) );
 	}
 }
--- 0x09-csharp-linear_algebra/28-determinant/28-main.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-main.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] matrix = { { 5, 0, -2 }, { 1, -8, 1 }, { 3, 2, 0 } };
+
+        Console.WriteLine(MatrixMath.Determinant(matrix));
+    }
+}
