1eeb5b75f62a5e7a7af5e2d15f38015327eccbf7
JHS1790 <1790@holbertonschool.com>
2021-05-25 14:53:51 -0500

    a

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MatrixMath
+{
+	public static double[,] Transpose(double[,] matrix)
+	{
+		if (matrix.GetLength(0) == 0)
+			return new double[0, 0];
+
+		double[,] result = new double[matrix.GetLength(1),matrix.GetLength(0)];
+
+		for (int i = 0; i < matrix.GetLength(0); i++)
+		{
+			for (int j = 0; j < matrix.GetLength(1); j++)
+			{
+				result[j, i] = matrix[i, j];
+			}
+		}
+
+		return result;
+	}
+}
