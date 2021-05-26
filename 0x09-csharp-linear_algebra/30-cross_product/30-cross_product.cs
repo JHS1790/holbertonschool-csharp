722e0d3b193c278b0886f0ce5d9b4f92304d6213
JHS1790 <1790@holbertonschool.com>
2021-05-26 09:46:26 -0500

    v

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -4,6 +4,9 @@ class VectorMath
 {
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return -1;
+            
         double ax = vector1[0], ay = vector1[1], az = vector1[2];
         double bx = vector2[0], by = vector2[1], bz = vector2[2];
 
